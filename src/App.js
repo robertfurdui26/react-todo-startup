e3be09b80ecafc6c9cbf5bd523a454b59633b2ae
TeofilSandu <eusebiu.f@menskin.co>
2023-02-27 10:06:24 +0200

    Upload image test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
-import ToDoItem from "./ToDoItem";
-import todoData from "./todoData";
-import "./style.css";
+import React from 'react';
+import ToDoItem from './ToDoItem';
+import todoData from './todoData';
+import './style.css';
 
 function App() {
   const todoNew = todoData.map((item) => (
     <ToDoItem key={item.id} item={item} />
   ));
-  return <div className="todo-list">{todoNew}</div>;
+
+  return (
+    <div className='todo-list'>
+      {todoNew}
+      <img src='./images/logo.svg' style={{ width: '40px', height: '40px' }} />
+    </div>
+  );
 }
 export default App;
