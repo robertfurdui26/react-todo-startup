0691398ea20975000facfac09775062b229cc49c
robertfurdui26 <125736128+robertfurdui26@users.noreply.github.com>
2023-02-28 04:51:31 -0800

    new changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,39 @@ import ToDoItem from "./ToDoItem";
 import todoData from "./todoData";
 import "./style.css";
 
-function App() {
-  const todoNew = todoData.map((item) => (
-    <ToDoItem key={item.id} item={item} />
-  ));
-  return <div className="todo-list">{todoNew}</div>;
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todoData,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(id) {
+    this.setState((prevState) => {
+      const updateTodos = prevState.todos.map((todos) => {
+        if (todos.id === id) {
+          return {
+            ...todos,
+            completed: !todos.completed,
+          };
+        }
+        return todos;
+      });
+
+      return {
+        todos: updateTodos,
+      };
+    });
+  }
+
+  render() {
+    const todoNew = this.state.todos.map((item) => (
+      <ToDoItem key={item.id} item={item} handleChange={this.handleChange} />
+    ));
+
+    return <div className="todo-list">{todoNew}</div>;
+  }
 }
 export default App;
