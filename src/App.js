9e0cfb2915a490b134c47d10103be0a3f09e5a86
robertfurdui26 <125736128+robertfurdui26@users.noreply.github.com>
2023-02-28 10:53:00 -0800

    comit again

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ToDoItem from './ToDoItem';
-import todoData from './todoData';
-import './style.css';
+import React from "react";
+import ToDoItem from "./ToDoItem";
+import todoData from "./todoData";
+import "./style.css";
 
-<<<<<<< HEAD
 class App extends React.Component {
   constructor() {
     super();
@@ -38,18 +37,5 @@ class App extends React.Component {
 
     return <div className="todo-list">{todoNew}</div>;
   }
-=======
-function App() {
-  const todoNew = todoData.map((item) => (
-    <ToDoItem key={item.id} item={item} />
-  ));
-
-  return (
-    <div className='todo-list'>
-      {todoNew}
-      <img src='./images/logo.svg' style={{ width: '40px', height: '40px' }} />
-    </div>
-  );
->>>>>>> e3be09b80ecafc6c9cbf5bd523a454b59633b2ae
 }
 export default App;
